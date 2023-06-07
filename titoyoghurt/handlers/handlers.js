b027b4513aa97d4bd2f80f5dcb647dd022a9335e
SolitaryDev <105029593+SolitaryDev001@users.noreply.github.com>
2023-06-06 23:36:10 -0400

    Delete titoyoghurt/handlers directory

--- titoyoghurt/handlers/handlers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-        onJoin(member) {
-                console.log("Entro " + member.user.tag);
-        },
-        onLeave(member) {
-                console.log("Salio " + member.user.tag);
-        }
-}
