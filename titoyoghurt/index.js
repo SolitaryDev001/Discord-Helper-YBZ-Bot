1ab07240d8a011e9a23a4416d958c4c9d8a75903
SolitaryDev <105029593+SolitaryDev001@users.noreply.github.com>
2023-06-06 23:35:48 -0400

    Update index.js
    New Index.js

--- titoyoghurt/index.js
+++ titoyoghurt/index.js
@@ -1,33 +1,13 @@
-const http = require("http");
-const { Client, Intents } = require("discord.js");
-const INFO = require("./utils/information.js");
-const COMMAND = require("./commands/command.js");
-const HANDLER = require("./handlers/handler.js");
-const PREFIX = INFO.PREFIX;
-const TOKEN = INFO.TOKEN;
-http.createServer((_, res) => res.end(`${PREFIX} Bot Online`)).listen(8080)
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-const client = new Client({
-        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+client.on('ready', () => {
+	require("./events/start.js").onStart(client);
 });
 
-console.log(`${PREFIX} Loading Bot...`);
-
-client.on("ready", () => {
-        console.log(`${PREFIX} Bot Successfully Started`);
-        client.user.setActivity("Jugando YoghurtBallZ");
+client.on('message', (msg) => {
+	require("./events/chat.js").onMessage(msg);
+	require("./events/commands.js").onMessage(msg);
 });
 
-client.on("messageCreate", (msg) => {
-        COMMAND.commands(msg);
-});
-
-client.on("guildMemberAdd", (member) => {
-        HANDLER.onJoin(member);
-});
-
-client.on("guildMemberRemove", (member) => {
-        HANDLER.onLeave(member);
-});
-
-client.login(TOKEN);
+client.login(require("./helpers/information.js").TOKEN);
